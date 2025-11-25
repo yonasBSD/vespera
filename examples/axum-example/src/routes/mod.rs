0ce7dbbc69f1d3e28d14591196b3c7930e38554b
owjs3901 <owjs3901@gmail.com>
2025-11-25 16:08:41 +0900

    Add complex

--- examples/axum-example/src/routes/mod.rs
+++ examples/axum-example/src/routes/mod.rs
@@ -50,7 +50,7 @@ pub async fn mod_file_with_struct_body(Json(body): Json<StructBody>) -> String {
     format!("name: {}, age: {}", body.name, body.age)
 }
 
-#[derive(Deserialize, Schema)]
+#[derive(Deserialize, Schema, Debug)]
 pub struct StructBodyWithOptional {
     pub name: Option<String>,
     pub age: Option<u32>,
@@ -61,4 +61,38 @@ pub async fn mod_file_with_struct_body_with_optional(
     Json(body): Json<StructBodyWithOptional>,
 ) -> String {
     format!("name: {:?}, age: {:?}", body.name, body.age)
+}
+
+#[derive(Deserialize, Schema)]
+#[serde(rename_all = "camelCase")]
+pub struct ComplexStructBody {
+    pub name: String,
+    pub age: u32,
+    pub nested_struct: StructBodyWithOptional,
+    pub array: Vec<String>,
+    pub map: HashMap<String, String>,
+    pub nested_array: Vec<StructBodyWithOptional>,
+    pub nested_map: HashMap<String, StructBodyWithOptional>,
+    pub nested_struct_array: Vec<StructBodyWithOptional>,
+    pub nested_struct_map: HashMap<String, StructBodyWithOptional>,
+    pub nested_struct_array_map: Vec<HashMap<String, StructBodyWithOptional>>,
+    pub nested_struct_map_array: HashMap<String, Vec<StructBodyWithOptional>>,
+}
+
+#[vespera::route(post, path = "/complex-struct-body")]
+pub async fn mod_file_with_complex_struct_body(Json(body): Json<ComplexStructBody>) -> String {
+    format!(
+        "name: {}, age: {}, nested_struct: {:?}, array: {:?}, map: {:?}, nested_array: {:?}, nested_map: {:?}, nested_struct_array: {:?}, nested_struct_map: {:?}, nested_struct_array_map: {:?}, nested_struct_map_array: {:?}",
+        body.name,
+        body.age,
+        body.nested_struct,
+        body.array,
+        body.map,
+        body.nested_array,
+        body.nested_map,
+        body.nested_struct_array,
+        body.nested_struct_map,
+        body.nested_struct_array_map,
+        body.nested_struct_map_array
+    )
 }
